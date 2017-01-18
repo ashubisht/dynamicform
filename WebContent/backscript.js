8499bc4eea04ea5f7c16f367bb6ee5505b38c890
ashubisht <utkarsh.bisht7@gmail.com>
2017-01-19 00:41:53 +0530

    Js changes

--- WebContent/backscript.js
+++ WebContent/backscript.js
@@ -10,23 +10,44 @@ app.controller("myctrl", function($scope){
 	
 	$scope.currentIndex=0;
 	$scope.maxIndex=10;
+	$scope.spaces=[];
+	
+	$scope.initspaces= function(){
+		for (var i =0; i<$scope.maxIndex;i++){
+			$scope.spaces.push(true);
+		}
+	}
+	
+	var getspace = function(){
+		for (var i =0; i<$scope.maxIndex;i++){
+			if($scope.spaces[i]){
+				return i;
+			}
+		}
+		return -1;
+	}
 	
 	$scope.add=function(){
 		if($scope.maxIndex> $scope.currentIndex){
-			++$scope.currentIndex;
-			var attributesObject={'getid':'inp'+$scope.currentIndex, 'getselid':'sel'+$scope.currentIndex};
-			$scope.tagfields.push(attributesObject);
+			var index = getspace();
+			if(index>=0){
+				var attributesObject={'getid':'inp'+index, 'getselid':'sel'+index};
+				$scope.tagfields.push(attributesObject);
+				$scope.spaces[index] = false;
+				++$scope.currentIndex;
+			}
 		}
-		
 	}
 	
 	$scope.rem= function(indexVal){
 		$scope.tagfields.splice(indexVal,1);
 		--$scope.currentIndex;
+		//Task 1: Need something to do with spaces in order to manage indexes
 	}
 	
 	$scope.save = function(){
 		console.log($scope.temporary);
+		//Task 2: Need to convert this temporary into array of objects which can be send to persistence
 	}
 	
 })
