c857f26dba76d611330f898448457b7e46d2e434
ashubisht <utkarsh.bisht7@gmail.com>
2017-02-07 17:52:06 +0530

    Changes in html and js. Directives use

--- WebContent/backscript.js
+++ WebContent/backscript.js
@@ -6,48 +6,40 @@ var app= angular.module("myapp",[]);
 app.controller("myctrl", function($scope){
 	
 	$scope.tagfields=[];
-	$scope.temporary={};
-	
-	$scope.currentIndex=0;
-	$scope.maxIndex=10;
-	$scope.spaces=[];
-	
-	$scope.initspaces= function(){
-		for (var i =0; i<$scope.maxIndex;i++){
-			$scope.spaces.push(true);
-		}
-	}
-	
-	var getspace = function(){
-		for (var i =0; i<$scope.maxIndex;i++){
-			if($scope.spaces[i]){
-				return i;
-			}
-		}
-		return -1;
-	}
 	
 	$scope.add=function(){
-		if($scope.maxIndex> $scope.currentIndex){
-			var index = getspace();
-			if(index>=0){
-				var attributesObject={'getid':'inp'+index, 'getselid':'sel'+index};
-				$scope.tagfields.push(attributesObject);
-				$scope.spaces[index] = false;
-				++$scope.currentIndex;
-			}
-		}
+		var dataobj = {
+				idmodel: '',
+				selectmodel: 'text'
+		};		
+		$scope.tagfields.push(dataobj);
 	}
 	
 	$scope.rem= function(indexVal){
 		$scope.tagfields.splice(indexVal,1);
-		--$scope.currentIndex;
-		//Task 1: Need something to do with spaces in order to manage indexes
 	}
 	
 	$scope.save = function(){
-		console.log($scope.temporary);
-		//Task 2: Need to convert this temporary object into array of objects with common keys which can be send to persistence
+		console.log($scope.tagfields);
 	}
 	
-})
+});
+
+app.directive('dynamicForm', [ '$rootScope', function($rootScope){
+	return{
+		restrict: 'E',
+		scope: {
+			dataObject:'=dataobject'
+		},
+		link: function(scope, element, attrs){
+			element.append(
+					'Sample directive ' + scope.dataObject.selectmodel
+					+ '<input type='+scope.selectmodel+' ng-model='+scope.dataObject.idmodel+'>'
+			);
+		}
+	}
+	
+}]);
+
+
+
